3a13efd4b21df77a3b2a2aef21adc637018b7d86
Rachel Mears <rachelmears@Rachels-MacBook-Air.local>
2020-09-28 11:51:02 -0400

    Monday, September 28

---
+++ Drawing.py
@@ -0,0 +1,31 @@
+from tkinter import Tk, Canvas, mainloop
+
+root = Tk()
+c = Canvas(root, width=500, height=500)
+c.pack()
+
+# Put drawing here!
+c.create_rectangle(0, 0, 500, 300, fill='blue')
+c.create_rectangle(0, 300, 500, 500, fill='green')
+c.create_rectangle(150, 300, 300, 400, fill='yellow')
+c.create_rectangle(200, 350, 250, 400, fill='red')
+c.create_polygon(100, 300, 225, 200, 350, 300, fill='red')
+c.create_oval(350, 100, 400, 150, fill='white')
+c.create_oval(375, 50, 425, 100, fill='white' )
+c.create_oval(400, 75, 500, 150, fill='white')
+c.create_oval(375, 125, 425, 175, fill='white')
+c.create_oval()
+
+for y in range(50, 500, 50):
+    c.create_line(0, y, 500, y)
+    c.create_text(18, y + 10, text=str(y))
+
+for x in range(50, 500, 50):
+    c.create_line(x, 0, x, 500)
+    c.create_text(x + 18, 10, text=str(x))
+
+def react_to_click(event):
+    root.quit()
+
+c.bind('<Button-1>', react_to_click)
+mainloop()
