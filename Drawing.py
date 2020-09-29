00d56ce61fd3f79130f7e49010df6f4526aba5c1
Rachel Mears <rachelmears@Rachels-MacBook-Air.local>
2020-09-29 14:33:24 -0400

    Tuesday, September 29

--- Drawing.py
+++ Drawing.py
@@ -14,15 +14,18 @@ c.create_oval(350, 100, 400, 150, fill='white')
 c.create_oval(375, 50, 425, 100, fill='white' )
 c.create_oval(400, 75, 500, 150, fill='white')
 c.create_oval(375, 125, 425, 175, fill='white')
-c.create_oval()
+c.create_oval(50, 50, 100, 100, fill='white')
+c.create_oval(75, 75, 150, 150, fill='white')
+c.create_oval(125, 100, 175, 150, fill='white')
 
-for y in range(50, 500, 50):
-    c.create_line(0, y, 500, y)
-    c.create_text(18, y + 10, text=str(y))
+# Lines
+# for y in range(50, 500, 50):
+#     c.create_line(0, y, 500, y)
+#     c.create_text(18, y + 10, text=str(y))
 
-for x in range(50, 500, 50):
-    c.create_line(x, 0, x, 500)
-    c.create_text(x + 18, 10, text=str(x))
+# for x in range(50, 500, 50):
+#     c.create_line(x, 0, x, 500)
+#     c.create_text(x + 18, 10, text=str(x))
 
 def react_to_click(event):
     root.quit()
