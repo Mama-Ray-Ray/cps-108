b8b09929b0f7a43b4a780aa1b1f93936d926fd80
Rachel Mears <rachelmears@Rachels-MacBook-Air.local>
2020-10-12 12:18:46 -0400

    Monday, October 12

--- drawing2.py
+++ drawing2.py
@@ -9,21 +9,22 @@ c.create_rectangle(25, 25, 50, 50, fill='black')
 c.create_rectangle(50, 50, 75, 75, fill='black')
 def checker1(x, y, height, width, fill='black'):
     c.create_rectangle(x,y, x+width, y+height, fill='black')
-def checker1_row(y):
-    for x in range(50, 450+1, 50):
+def checker1_row(y, start):
+    for x in range(start, 500+1, 50):
         checker1(x,y, 25, 25, 'black')
-for y in range(50, 450+1, 50):
-    checker1_row(y)
-def checker2(x, y, height, width, fill='black'):
+for y in range(50, 500, 50):
+    checker1_row(y, 50)
+for y in range(25, 500, 50):
+    checker1_row(y, 25)
 
 # Lines
-for y in range(50, 500, 50):
-    c.create_line(0, y, 500, y)
-    c.create_text(18, y + 10, text=str(y))
+# for y in range(50, 500, 50):
+#     c.create_line(0, y, 500, y)
+#     c.create_text(18, y + 10, text=str(y))
 
-for x in range(50, 500, 50):
-    c.create_line(x, 0, x, 500)
-    c.create_text(x + 18, 10, text=str(x))
+# for x in range(50, 500, 50):
+#     c.create_line(x, 0, x, 500)
+#     c.create_text(x + 18, 10, text=str(x))
 
 def react_to_click(event):
     root.quit()
